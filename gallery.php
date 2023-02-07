d75cfd765a8dfca108e604dd78217b46bf37f221
Mondestin <sydneymondestin@gmail.com>
2023-02-07 23:01:34 +0100

    merge steph and template

---
+++ gallery.php
@@ -0,0 +1,300 @@
+
+<!doctype html>
+<html class="no-js" lang="zxx">
+
+<head>
+
+    <!-- Title -->
+    <title>Gallery</title>
+    <?php include_once('head.php') ?>
+</head>
+
+<body>
+
+
+    <div class="ai-preloader active">
+        <div class="ai-preloader-inner h-100 d-flex align-items-center justify-content-center">
+            <div class="ai-child ai-bounce1"></div>
+            <div class="ai-child ai-bounce2"></div>
+            <div class="ai-child ai-bounce3"></div>
+        </div>
+    </div>
+
+    <!-- Main Wrapper Start -->
+    <div class="wrapper">
+        
+    <?php include_once('navbar.php') ?>
+    
+      
+        <!-- Mobile Header area End -->
+
+        <!-- Breadcrumb area Start -->
+        <div class="breadcrumb-area bg--white-6 pt--60 pb--70 pt-lg--40 pb-lg--50 pt-md--30 pb-md--40 ">
+            <div class="container-fluid mt--145">
+                <div class="row">
+                    <div class="col-12 text-center">
+                        <h1 class="page-title">Collections</h1>
+                        <ul class="breadcrumb justify-content-center">
+                            <li>take a great look on our collection</li>
+                          
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Breadcrumb area End -->
+
+        <!-- Main Content Wrapper Start -->
+        <div id="content" class="main-content-wrapper">
+            <div class="shop-page-wrapper">
+                <div class="container-fluid">
+                    <div class="row shop-fullwidth pt--45 pt-md--35 pt-sm--20 pb--60 pb-md--50 pb-sm--40">
+                        <div class="col-12">
+                            <div class="shop-toolbar">
+                    
+                            
+                            </div>
+
+
+
+
+
+
+
+
+
+                            <div class="shop-products">
+                                <div class="row grid-space-20 xxl-block-grid-5">
+                                  
+                                <div class="col-lg-3 col-sm-6 mb--40 mb-md--30">
+                                        <div class="airi-product">
+                                            <div class="product-inner">
+                                                <figure class="product-image">
+                                                    <div class="product-image--holder">
+                                                        <a href="product-details.html">
+                                                            <img src="assets/img/products/prod-19-4.webp"
+                                                                alt="Product Image" class="primary-image">
+                                                            <img src="assets/img/products/prod-19-1.webp"
+                                                                alt="Product Image" class="secondary-image">
+                                                        </a>
+                                                    </div>
+                                                    <div class="airi-product-action">
+                                                        <div class="product-action">
+                                                            <a class="quickview-btn action-btn" data-bs-toggle="tooltip"
+                                                                data-bs-placement="left" title="Quick Shop">
+                                                                <span data-bs-toggle="modal" data-bs-target="#productModal">
+                                                                    <i class="fa fa-eye"></i>
+                                                                </span>
+                                                            </a>
+                                                          
+                                                            <a class="add_wishlist action-btn" href="wishlist.html"
+                                                                data-bs-toggle="tooltip" data-bs-placement="left"
+                                                                title="Add to Wishlist">
+                                                                <i class="fa fa-heart"></i>
+                                                            </a>
+                                                           
+                                                        </div>
+                                                    </div>
+                                                    <span class="product-badge new">New</span>
+                                                </figure>
+                                                <div class="product-info text-center">
+                                                    <h3 class="product-title">
+                                                        <a href="product-details.html">Hig-Rise Skinny Jean</a>
+                                                    </h3>
+                                                    <div class="product-rating">
+                                                        <span>
+                                                            <i class="dl-icon-star rated"></i>
+                                                            <i class="dl-icon-star rated"></i>
+                                                            <i class="dl-icon-star rated"></i>
+                                                            <i class="dl-icon-star"></i>
+                                                            <i class="dl-icon-star"></i>
+                                                        </span>
+                                                    </div>
+                                            
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="col-lg-3 col-sm-6 mb--40 mb-md--30">
+                                        <div class="airi-product">
+                                            <div class="product-inner">
+                                                <figure class="product-image">
+                                                    <div class="product-image--holder">
+                                                        <a href="product-details.html">
+                                                            <img src="assets/img/products/prod-19-4.webp"
+                                                                alt="Product Image" class="primary-image">
+                                                            <img src="assets/img/products/prod-19-1.webp"
+                                                                alt="Product Image" class="secondary-image">
+                                                        </a>
+                                                    </div>
+                                                    <div class="airi-product-action">
+                                                        <div class="product-action">
+                                                            <a class="quickview-btn action-btn" data-bs-toggle="tooltip"
+                                                                data-bs-placement="left" title="Quick Shop">
+                                                                <span data-bs-toggle="modal" data-bs-target="#productModal">
+                                                                    <i class="fa fa-eye"></i>
+                                                                </span>
+                                                            </a>
+                                                          
+                                                            <a class="add_wishlist action-btn" href="wishlist.html"
+                                                                data-bs-toggle="tooltip" data-bs-placement="left"
+                                                                title="Add to Wishlist">
+                                                                <i class="fa fa-heart"></i>
+                                                            </a>
+                                                           
+                                                        </div>
+                                                    </div>
+                                                    <span class="product-badge new">New</span>
+                                                </figure>
+                                                <div class="product-info text-center">
+                                                    <h3 class="product-title">
+                                                        <a href="product-details.html">Hig-Rise Skinny Jean</a>
+                                                    </h3>
+                                                    <div class="product-rating">
+                                                        <span>
+                                                            <i class="dl-icon-star rated"></i>
+                                                            <i class="dl-icon-star rated"></i>
+                                                            <i class="dl-icon-star rated"></i>
+                                                            <i class="dl-icon-star"></i>
+                                                            <i class="dl-icon-star"></i>
+                                                        </span>
+                                                    </div>
+                                              
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="col-lg-3 col-sm-6 mb--40 mb-md--30">
+                                        <div class="airi-product">
+                                            <div class="product-inner">
+                                                <figure class="product-image">
+                                                    <div class="product-image--holder">
+                                                        <a href="product-details.html">
+                                                            <img src="assets/img/products/prod-19-4.webp"
+                                                                alt="Product Image" class="primary-image">
+                                                            <img src="assets/img/products/prod-19-1.webp"
+                                                                alt="Product Image" class="secondary-image">
+                                                        </a>
+                                                    </div>
+                                                    <div class="airi-product-action">
+                                                        <div class="product-action">
+                                                            <a class="quickview-btn action-btn" data-bs-toggle="tooltip"
+                                                                data-bs-placement="left" title="Quick Shop">
+                                                                <span data-bs-toggle="modal" data-bs-target="#productModal">
+                                                                    <i class="fa fa-eye"></i>
+                                                                </span>
+                                                            </a>
+                                                          
+                                                            <a class="add_wishlist action-btn" href="wishlist.html"
+                                                                data-bs-toggle="tooltip" data-bs-placement="left"
+                                                                title="Add to Wishlist">
+                                                                <i class="fa fa-heart"></i>
+                                                            </a>
+                                                           
+                                                        </div>
+                                                    </div>
+                                                    <span class="product-badge new">New</span>
+                                                </figure>
+                                                <div class="product-info text-center">
+                                                    <h3 class="product-title">
+                                                        <a href="product-details.html">Hig-Rise Skinny Jean</a>
+                                                    </h3>
+                                                    <div class="product-rating">
+                                                        <span>
+                                                            <i class="dl-icon-star rated"></i>
+                                                            <i class="dl-icon-star rated"></i>
+                                                            <i class="dl-icon-star rated"></i>
+                                                            <i class="dl-icon-star"></i>
+                                                            <i class="dl-icon-star"></i>
+                                                        </span>
+                                                    </div>
+                                            
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+
+
+                                    <div class="col-lg-3 col-sm-6 mb--40 mb-md--30">
+                                        <div class="airi-product">
+                                            <div class="product-inner">
+                                                <figure class="product-image">
+                                                    <div class="product-image--holder">
+                                                        <a href="product-details.html">
+                                                            <img src="assets/img/products/prod-19-4.webp"
+                                                                alt="Product Image" class="primary-image">
+                                                            <img src="assets/img/products/prod-19-1.webp"
+                                                                alt="Product Image" class="secondary-image">
+                                                        </a>
+                                                    </div>
+                                                    <div class="airi-product-action">
+                                                        <div class="product-action">
+                                                            <a class="quickview-btn action-btn" data-bs-toggle="tooltip"
+                                                                data-bs-placement="left" title="Quick Shop">
+                                                                <span data-bs-toggle="modal" data-bs-target="#productModal">
+                                                                    <i class="fa fa-eye"></i>
+                                                                </span>
+                                                            </a>
+                                                          
+                                                            <a class="add_wishlist action-btn" href="wishlist.html"
+                                                                data-bs-toggle="tooltip" data-bs-placement="left"
+                                                                title="Add to Wishlist">
+                                                                <i class="fa fa-heart"></i>
+                                                            </a>
+                                                           
+                                                        </div>
+                                                    </div>
+                                                    <span class="product-badge new">New</span>
+                                                </figure>
+                                                <div class="product-info text-center">
+                                                    <h3 class="product-title">
+                                                        <a href="product-details.html">Hig-Rise Skinny Jean</a>
+                                                    </h3>
+                                                    <div class="product-rating">
+                                                        <span>
+                                                            <i class="dl-icon-star rated"></i>
+                                                            <i class="dl-icon-star rated"></i>
+                                                            <i class="dl-icon-star rated"></i>
+                                                            <i class="dl-icon-star"></i>
+                                                            <i class="dl-icon-star"></i>
+                                                        </span>
+                                                    </div>
+                                         
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <nav class="pagination-wrap">
+                                <ul class="pagination">
+                                    <li><a href="#" class="prev page-number"><i class="fa fa-angle-double-left"></i></a>
+                                    </li>
+                                    <li><span class="current page-number">1</span></li>
+                                    <li><a href="#" class="page-number">2</a></li>
+                                    <li><a href="#" class="page-number">3</a></li>
+                                    <li><a href="#" class="next page-number"><i
+                                                class="fa fa-angle-double-right"></i></a></li>
+                                </ul>
+                            </nav>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Main Content Wrapper Start -->
+
+        <?php include_once('footer.php') ?>
+
+    </div>
+    <!-- Main Wrapper End -->
+
+
+    <?php include_once('scripts.php') ?>
+
+
+</body>
+
+</html>
