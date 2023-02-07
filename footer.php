35fec042daa63a5ac0b5f62f9203a1ba7d4b2790
KADDOURI Chaima <chaima.kaddouri@estiam.com>
2023-02-07 12:08:03 +0100

    fisrt commit

---
+++ footer.php
@@ -0,0 +1,72 @@
+
+<!-- Footer Start Here -->
+<footer class="footer footer-3">
+            <div class="container-fluid">
+                <div class="row align-items-center pt--25">
+                    <div class="col-md-3 text-center text-md-start">
+                        <div class="footer-widget mb--15">
+                            <div class="textwidget">
+                                <a href="index.html" class="footer-logo">
+                                    <img src="assets/img/logo/logo.svg" alt="Logo">
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6 text-center">
+                        <div class="footer-widget mb--15">
+                            <ul class="contact-info contact-info--2">
+                                <li class="contact-info__item">
+                                    <i class="fa fa-phone"></i>
+                                    <span><a href="tel:+012345 6788" class="contact-info__link">(+012) 345 6788</a></span>
+                                </li>
+                                <li class="contact-info__item">
+                                    <i class="fa fa-envelope"></i>
+                                    <span><a href="mailto:demo@email.com" class="contact-info__link">demo@email.com</a></span>
+                                </li>
+                                <li class="contact-info__item">
+                                    <i class="fa fa-map-marker"></i>
+                                    <span>Example Address</span>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-md-3 text-center text-md-end">
+                        <!-- Social Icons Start Here -->
+                        <ul class="social social-small">
+                            <li class="social__item">
+                                <a href="https://twitter.com" class="social__link">
+                                    <i class="fa fa-twitter"></i>
+                                </a>
+                            </li>
+                            <li class="social__item">
+                                <a href="https://plus.google.com" class="social__link">
+                                    <i class="fa fa-google-plus"></i>
+                                </a>
+                            </li>
+                            <li class="social__item">
+                                <a href="https://facebook.com" class="social__link">
+                                    <i class="fa fa-facebook"></i>
+                                </a>
+                            </li>
+                            <li class="social__item">
+                                <a href="https://youtube.com" class="social__link">
+                                    <i class="fa fa-youtube"></i>
+                                </a>
+                            </li>
+                            <li class="social__item">
+                                <a href="https://instagram.com" class="social__link">
+                                    <i class="fa fa-instagram"></i>
+                                </a>
+                            </li>
+                        </ul>
+                        <!-- Social Icons End Here -->
+                    </div>
+                </div>
+                <div class="row pt--15 pb--15">
+                    <div class="col-12 text-center">
+                        <p class="copyright-text">&copy; 2018 AIRI All rights reserved. Designed by LA-STUDIO</p>
+                    </div>
+                </div>
+            </div>
+        </footer>
+        <!-- Footer End Here -->
